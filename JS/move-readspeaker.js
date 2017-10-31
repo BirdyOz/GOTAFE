ea2ec8c2c57bacccb7e695d1055eb0a60c2873a7
BirdyOz <greg.bird.oz@gmail.com>
2017-11-01 09:44:47 +1100

    2017-11-01 09:44:45

--- JS/move-readspeaker.js
+++ JS/move-readspeaker.js
@@ -2,11 +2,10 @@
 * @Author: Greg Bird (@BirdyOz, greg.bird.oz@gmail.com)
 * @Date:   2017-10-30 09:21:38
 * @Last Modified by:   BirdyOz
-* @Last Modified time: 2017-11-01 09:38:32
+* @Last Modified time: 2017-11-01 09:44:29
 */
 
 $(function() {
     var RS_player = $('.block_readspeaker_embhl').detach();
-    console.log("@GB: RS_player = ", RS_player);
-    $('#region-main').prepend(RS_player);
+    $('#region-main').before(RS_player);
 });
