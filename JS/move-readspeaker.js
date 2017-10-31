f69e1e1618f2c8e7c9b90726334e8abbac88f56b
BirdyOz <greg.bird.oz@gmail.com>
2017-11-01 09:30:41 +1100

    First commit

---
+++ JS/move-readspeaker.js
@@ -0,0 +1,11 @@
+/*
+* @Author: Greg Bird (@BirdyOz, greg.bird.oz@gmail.com)
+* @Date:   2017-10-30 09:21:38
+* @Last Modified by:   BirdyOz
+* @Last Modified time: 2017-10-30 09:49:17
+*/
+
+$(function() {
+    var RS_player = $('.block_readspeaker_embhl').detach();
+    $('#snap-course-footer-about').before(RS_player);
+});
